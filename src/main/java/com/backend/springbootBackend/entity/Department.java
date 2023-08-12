87ed248df7cffa90872b0cc59e0deb6429329d48
PumuduRajakaruna <e19306@eng.pdn.ac.lk>
2023-08-12 18:30:19 +0530

    Add hibernate validation

--- src/main/java/com/backend/springbootBackend/entity/Department.java
+++ src/main/java/com/backend/springbootBackend/entity/Department.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
 @Entity
@@ -12,6 +14,7 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+    @NotBlank(message = "Please enter the department name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
